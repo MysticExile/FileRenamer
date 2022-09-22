4510e86382f36eac5200202dae4ba9ba300fd5b0
MysticExile <ethan.baboeram@gmail.com>
2022-09-23 00:00:27 +0200

    Revamp of the program

--- Program.cs
+++ Program.cs
@@ -7,41 +7,18 @@ namespace FileRenamer
     {
         static void Main(string[] args)
         {
+            Renamer renamer = new Renamer();
             //ask user for folder path where the files are located
             Console.WriteLine("Folder path: ");
-            string folderPath = Console.ReadLine();
-            if (folderPath == "")
+            renamer.FolderPath = Console.ReadLine();
+            if (renamer.FolderPath == "")
             {
                 Console.WriteLine("Path cannot be empty");
             }
             else
             {
-                string[] allFiles = Directory.GetFiles(folderPath, "*.*", SearchOption.TopDirectoryOnly);
-                if (allFiles.Length < 1)
-                {
-                    Console.WriteLine("Incorrect path and/or folder is empty");
-                }
-                else
-                {
-                    //ask user to what the files should be renamed
-                    Console.WriteLine("What would you like to rename the files to?");
-                    string renamedFilenamebase = Console.ReadLine();
-                    //go through the array, gather necessary info, rename files
-                    for (int i = 0; i < allFiles.Length; i++)
-                    {
-                        FileInfo fi = new FileInfo(allFiles[i]);
-                        string fileExtension = fi.Extension;
-                        string fileNumber = "0";
-                        if (i < 10)
-                        {
-                            fileNumber = i.ToString();
-                            fileNumber = "0" + fileNumber;
-                        }
-                        string renamedFilenames = folderPath + "\\" + renamedFilenamebase + " " + fileNumber + fileExtension;
-                        System.IO.File.Move(allFiles[i], renamedFilenames);
-                    }
-                    Console.WriteLine("Done!");
-                }
+                string[] allFiles = Directory.GetFiles(renamer.FolderPath, "*.*", SearchOption.TopDirectoryOnly);
+                renamer.rename(allFiles);
             }
             Console.WriteLine("Press any key to close...");
             Console.ReadLine();
---
+++ Renamer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRenamer
+{
+    internal class Renamer
+    {
+        private string fileExtension;
+        private string fileNumber;
+        private string renamedFileName;
+        private string oldFileName;
+        private int seasonNumber;
+        private string folderPath;
+
+        public string FileExtension { get => fileExtension; set => fileExtension = value; }
+        public string OldFileName { get => oldFileName; set => oldFileName = value; }
+        public string FileNumber { get => fileNumber; set => fileNumber = value; }
+        public string RenamedFileName { get => renamedFileName; set => renamedFileName = value; }
+        public int SeasonNumber { get => seasonNumber; set => seasonNumber = value; }
+        public string FolderPath { get => folderPath; set => folderPath = value; }
+
+        public void rename(string[] files)
+        {
+            //check if the included path exists or if it has contents
+            if (files.Length < 1)
+            {
+                Console.WriteLine("Incorrect path and/or folder is empty");
+            }
+            else if (files.Length > 0)
+            {
+                //ask user to what the files should be renamed
+                Console.WriteLine("What is the name of the show?");
+                OldFileName = Console.ReadLine();
+                Console.WriteLine("What season is this?");
+                SeasonNumber = Convert.ToInt32(Console.ReadLine());
+                //go through the array, gather necessary info, rename files
+                for (int i = 0; i < files.Length; i++)
+                {
+                    FileInfo fi = new FileInfo(files[i]);
+                    FileExtension = fi.Extension;
+                    FileNumber = (i + 1).ToString();
+                    if((i+1) < 10)
+                    {
+                        FileNumber = "0" + FileNumber;
+                        Console.WriteLine(FileNumber);
+                    }
+                    RenamedFileName = folderPath + "\\" + OldFileName + " Season " + SeasonNumber + " Episode " + FileNumber + FileExtension;
+                    File.Move(files[i], RenamedFileName);
+                }
+                Console.WriteLine("Done!");
+            }
+        }
+    }
+}
